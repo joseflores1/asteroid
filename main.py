a68485dd9c483fc4ed7e97821d4ccfa6cb868805
joseflores1 <joseflores@alumnos.uai.cl>
2025-01-04 21:13:39 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
